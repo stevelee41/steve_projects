b05244f4215334f907867ce445f4af516f4865ce
stevelee41 <stevelee41@gmail.com>
2017-01-17 20:30:05 -0500

    Create calculators.js

---
+++ calculators.js
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>My test file</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="mystyles.css">
+    </head>
+
+    <body>
+        
+    <!--////////////////////////////////////////////////////////////////////////
+    //  
+    //                                      NAVBAR
+    //
+    ////////////////////////////////////////////////////////////////////////////
+    -->
+    <!--////////////////////////////////////////////////////////////////////////
+    //  
+    //                                END NAVBAR
+    //
+    /////////////////////////////////////////////////////////////////////////-->
+
+        <h2 class="text-center">My Calculators</h2>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12">
+                    <ul class="calculator_list">
+                        <li><a class="calculator_select center-block" href="corrected_calcium.html">Corrected Calcium</a></li>
+                        <li><a class="calculator_select center-block" href="creatinine_clearance.html">Creatinine Clearance</a></li>
+                        <li><a class="calculator_select center-block" href="CURB65.html">CURB65</a></li>
+                        <li><a class="calculator_select center-block" href="SIRS_crireria.html">SIRS Criteria</a></li>
+                        <li><a class="calculator_select center-block" href="Wells_score.html">Wells Score</a></li>
+                    </ul>
+                </div>
+            </div>    
+        </div>              
+        
+        
+        <script src=”mymedapps.js”>  </script>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+    
+</html>
+
+
